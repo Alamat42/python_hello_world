d97c146a5073813dac0a1749601eb31cae45b280
Igor Danilets <danilec.igor@gmail.com>
2025-12-17 01:19:40 +0300

    lesson 21

---
+++ 11_18873.py
@@ -0,0 +1,7 @@
+id = 70 * 1024 / 345
+# M = 487 + 25 = 512
+# 2 ** 9 = 512
+# size_char = 9 bit
+# id_size >= 108 byte 
+print(id)
+print(208 * 8 / 9)
---
+++ 11_25350.py
@@ -0,0 +1,9 @@
+# 65536 = 2 ** 16
+# 105 символов
+# 7 Мбайт
+
+id_size = 7 * 1024 * 1024 / (65536)
+print(id_size)
+char_size = id_size * 8 / 105
+print(char_size)
+print(2 ** 9)
